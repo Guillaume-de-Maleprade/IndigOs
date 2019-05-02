532a75a0a854f61f78750a3befb64b8d1c9c8358
Guillaume de Maleprade <g.de.maleprade@gmail.com>
2019-05-02 15:05:12 +0200

    gitignore update

--- 7-HORLOGE/Init_GDT.h
+++
@@ -1,6 +0,0 @@
-#ifndef INIT_GDT_H
-#define	INIT_GDT_H
-void Initialisation_GDT();
-
-#endif	/* INIT_GDT_H */
-
--- 7-HORLOGE/build/Debug/MinGW_ELF-Windows/DELAY.o.d
+++
@@ -1,5 +0,0 @@
-build/Debug/MinGW_ELF-Windows/DELAY.o: DELAY.c type.h DELAY.h
-
-type.h:
-
-DELAY.h:
